b65892b0572cd032d98a5d94c97e2c6386e9b29e
Avrgebro <jabcarranz@gmail.com>
2019-06-24 20:21:16 -0500

    nuevo template

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: 'SG - La Garita',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
